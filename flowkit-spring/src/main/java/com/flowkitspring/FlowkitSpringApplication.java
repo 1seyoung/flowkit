bba42c8952554a390ca1eeacaaa9acabe9f407f1
1seyoung <akdlrnal0705@gmail.com>
2025-04-03 01:21:02 +0900

    Init

---
+++ flowkit-spring/src/main/java/com/flowkitspring/FlowkitSpringApplication.java
@@ -0,0 +1,13 @@
+package com.flowkitspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowkitSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowkitSpringApplication.class, args);
+	}
+
+}
---
+++ flowkit-spring/src/test/java/com/flowkitspring/FlowkitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowkitspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowkitSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
